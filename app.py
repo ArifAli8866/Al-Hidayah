af3bf0108fe356d7687995dfb6190d3c7f3bb727
Arif Ali <2arif2143055@gmail.com>
2026-05-23 01:26:34 +0500

    Update app.py

--- app.py
+++ app.py
@@ -425,7 +425,7 @@ Rules:
             genai.configure(api_key=GEMINI_KEY)
 
             model = genai.GenerativeModel(
-                model_name="gemini-1.5-flash-latest",
+                model_name="gemini-1.5-pro",
                 system_instruction=system_prompt
             )
 
